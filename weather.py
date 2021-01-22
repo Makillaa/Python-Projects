b51776ada924c4a59b93326519145e3667e27ea0
Makillaa <1999gmo@gmail.com>
2021-01-22 15:16:00 +0200

    Translated comments

--- weather.py
+++ weather.py
@@ -6,7 +6,7 @@ from pyowm.utils.config import get_default_config
 config_dict = get_default_config()
 config_dict['language'] = 'ru'
 city = input('Погода какого города вас интересует?: ')
-owm = OWM('your-api-key', config_dict) #для получания API ключа: https://home.openweathermap.org/api_keys
+owm = OWM('your-api-key', config_dict) #for get api key use: https://home.openweathermap.org/api_keys
 mgr = owm.weather_manager()
 
 observation = mgr.weather_at_place(city)
@@ -14,5 +14,5 @@ weather = observation.weather
 temperature = weather.temperature('celsius')['temp']
 detail = weather.detailed_status
 
-print('В городе ' + city + ' сейчас температура: ' + str(round(temperature)) + u'\xb0C') #Только для кодировки 850(CMD, Win)
+print('В городе ' + city + ' сейчас температура: ' + str(round(temperature)) + u'\xb0C') #For encoding only 850(CMD, Win)
 print('Так же в указанном городе ' + str(detail))
